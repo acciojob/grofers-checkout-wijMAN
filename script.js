f826b87bae31cddea663935503f1f8d2cd6d1ec4
Wijdan <66134000+wijMAN@users.noreply.github.com>
2023-02-21 20:39:45 +0530

    created a submission

--- script.js
+++ script.js
@@ -4,7 +4,23 @@ document.body.appendChild(getSumBtn);
 
 const getSum = () => {
 //Add your code here
-  
+  let a = document.getElementsByClassName("price");
+  let b = 0;
+
+  let z = Array.from(a);
+  z.forEach(function (item) {
+    b += parseInt(item.innerHTML);
+  });
+
+  //   for (let i = 0; i < a.length; i++) {
+  //     b += parseInt(a[i].innerHTML);
+  //   }
+  document.querySelector("table").innerHTML += `
+    <tr>
+      <td >${b}</td>
+    </tr>
+  `;
+  getSumBtn.disabled = true;
 };
 
 getSumBtn.addEventListener("click", getSum);
